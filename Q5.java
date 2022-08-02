c193d6a7121d3324efcffd96c0120951ca6478e8
isharjotsingh <2006906.isharjotsinghmann@gmail.com>
2022-08-02 09:48:12 +0530

    jfyh

--- Q5.java
+++
@@ -1,9 +0,0 @@
-// Q no. - 5
-/*
- wrapper class is the class which contain primitive datatypes and used it as a object.
- types -  boxing , Unboxing
-
- example -: int = Integer       {where int, char, boolean is primitive datatypes and
-           char = Character                        Integer, Charcter, Boolean is wrapper classes}
-           boolean = Boolean, etc
-           */
